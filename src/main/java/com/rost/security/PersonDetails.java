2dd9f2d33bd4442c3a2e591c710e62cf038fe661
Rost <coderost@yandex.ru>
2023-01-21 15:29:45 +0300

    Create security wrapper for model.

---
+++ src/main/java/com/rost/security/PersonDetails.java
@@ -0,0 +1,50 @@
+package com.rost.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.rost.models.Person;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class PersonDetails implements UserDetails {
+    private final Person person;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return person.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
