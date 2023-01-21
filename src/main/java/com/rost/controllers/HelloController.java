8dc8887bad6c15e853184bea3c5bc43eb32dace7
Rost <coderost@yandex.ru>
2023-01-21 16:44:22 +0300

    Add new method in HelloController and corresponding view.

--- src/main/java/com/rost/controllers/HelloController.java
+++ src/main/java/com/rost/controllers/HelloController.java
@@ -1,12 +1,27 @@
 package com.rost.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.rost.security.PersonDetails;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "hello";
+    }
+
+    @GetMapping("/show-user-info")
+    public ModelAndView showUserInfo() {
+        /**
+         * Spring достаёт объект из сессии и кладёт его в context.
+         */
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        return new ModelAndView("user", "person", personDetails.getPerson());
     }
 }
